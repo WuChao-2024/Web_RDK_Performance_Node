e7d2fcae8ac29ec2e9e3bf7514320ff5d1e9e34c
root <root@ubuntu.net>
2024-03-05 19:09:14 +0800

    add gitignote

--- README.md
+++ README.md
@@ -4,9 +4,9 @@
 
 该应用基于Web网页打造，无论是什么品牌的电脑和手机，只需要在浏览器访问即可。
 
-<img src=".\desktop_demo.jpg" alt="desktop_demo" style="zoom:70%;" />
+<img src=".\doc\desktop_demo.jpg" alt="desktop_demo" style="zoom:70%;" />
 
-<img src=".\mult_device.jpg" alt="mult_device" style="zoom:70%;" />
+<img src=".\doc\mult_device.jpg" alt="mult_device" style="zoom:70%;" />
 
 安装requirements，主要是Flask和pstiul
 
@@ -27,4 +27,32 @@ python3 app.py
 ```
 
 
+
+
+### BPU调频策略
+查看BPU支持的调频策略
+```bash
+cat /sys/devices/system/bpu/bpu0/devfreq/devfreq1/available_governors
+cat /sys/devices/system/bpu/bpu1/devfreq/devfreq1/available_governors
+```
+更改BPU模式
+bpu_ondemand powersave performance simple_ondemand
+```bash
+sudo bash -c "echo performance > /sys/devices/system/bpu/bpu0/devfreq/devfreq1/governor"
+sudo bash -c "echo performance > /sys/devices/system/bpu/bpu1/devfreq/devfreq2/governor"
+```
+
+### CPU调频策略
+```bash
+cat /sys/devices/system/cpu/cpufreq/policy0/scaling_available_governors
+```
+更改CPU模式
+conservative: 需要时逐级调整
+ondemand: 需要时立刻调整
+powersave: 节能
+performance: 性能
+```bash
+sudo bash -c "echo performance > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor"
+sudo bash -c "echo conservative > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor"
+```
 
--- app.py
+++ app.py
@@ -14,18 +14,20 @@
 
 from flask import Flask, render_template
 from psutil import cpu_percent, virtual_memory, disk_usage
-# 创建一个app对象
-app = Flask(__name__)
+import argparse
 
-# 根路由
+DEVICE_NAME = "rdkx3"
+DEVICE_NUM = 0 # 0:rdkx3, 1:rdkultra
+
+# Flask app
+app = Flask(__name__)
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template(DEVICE_NAME + ".html")
 
-# 根路由
 @app.route("/wide")
 def index_wide():
-    return render_template("index_wide.html")
+    return render_template(DEVICE_NAME + "_wide.html")
 
 # 请求CPU, BPU, Memory, Tempture 信息（快速）
 @app.route("/getState")
@@ -42,8 +44,8 @@ def getState():
     stateString += "%012d,%012d,"%(memorys[3], memorys[1])
     
     ## BPU  ## Temp
-    bpu0 = open('/sys/devices/system/bpu/bpu0/ratio', 'r', encoding='utf-8')
-    bpu1 = open('/sys/devices/system/bpu/bpu1/ratio', 'r', encoding='utf-8')
+    bpu0 = open('/sys/DEVICE_NAMEs/system/bpu/bpu0/ratio', 'r', encoding='utf-8')
+    bpu1 = open('/sys/DEVICE_NAMEs/system/bpu/bpu1/ratio', 'r', encoding='utf-8')
     cpu_temp = open('/sys/class/hwmon/hwmon0/temp1_input', 'r',  encoding='utf-8')
     stateString += "%03d,%03d,"%(int(bpu0.read()), int(bpu1.read()))
     stateString += cpu_temp.read()[0:5]
@@ -63,4 +65,25 @@ def getDisk():
     return disk_info_string
 
 if __name__ == "__main__":
+    # 设备判断(根据设备树)
+    tree = open('/sys/firmware/DEVICE_NAMEtree/base/model', 'r', encoding='utf-8').read()
+    if "X3" in tree :
+        DEVICE_NAME = "rdkx3"
+        DEVICE_NUM = 0
+    elif "Journey 5" in tree:
+        DEVICE_NAME = "rdkultra"
+        DEVICE_NUM = 1
+    elif "X5" in tree:
+        DEVICE_NAME = "rdkx5"
+        DEVICE_NUM = 2
+    else:
+        print("Your device didn't support.")
+        exit()
+
+    # 用户输入
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device', type=str, default='-1', help='0: RDK X3\n1: RDK Ultra\n2: RDK X5\n')
+    opt = parser.parse_args()
+
+    # 启动应用
     app.run(debug=True, host="0.0.0.0")
